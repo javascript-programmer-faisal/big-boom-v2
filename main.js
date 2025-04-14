5938be4d1560dc0f09e917f1e7e422d8594605bf
mern-coder-faisal <js.dev.faisal@gmail.com>
2025-04-14 12:14:43 +0600

    finish

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ window.onload = () => {
       }
     });
   };
+
+  function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar.style.width === "250px") {
+        sidebar.style.width = "0";
+    } else {
+        sidebar.style.width = "250px";
+    }
+}
